dd4f9a09cf75616c1e62fec9ab00102a4360fd0d
Camilo Gonzalez <camilo.gdonoso@gmail.com>
2022-10-05 08:51:54 -0400

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+// import { ContadorComponent } from './contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+// import { HeroeComponent } from './heroes/heroe/heroe.component';
+// import { ListadoComponent } from './heroes/listado/listado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    // ContadorComponent,
+    // HeroeComponent,
+    // ListadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    ContadorModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
