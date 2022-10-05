3faf0cdc1c295d0f9424f911c70081945b9e7d42
Camilo Gonzalez <camilo.gdonoso@gmail.com>
2022-10-05 16:38:45 -0400

    agregar ngfor para mostrar datos dinamicos y crear nuevo elemento

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -9,14 +9,37 @@ interface Personaje {
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 2500
+    }
+  ];
   nuevo: Personaje = {
-    nombre: 'Truncks',
-    poder: 600
+    nombre: '',
+    poder: 0
   }
+nombre: any;
+poder: any;
   cambiarNombre(event: any) {
     console.log(event);
   }
 agregar(){
+  if(this.nuevo.nombre.trim().length === 0) {
+    return;
+  }
+  this.personajes.push(this.nuevo);
+
+  this.nuevo = {
+    nombre: '',
+    poder: 0
+  }
+
 console.log(this.nuevo);
 }
 
